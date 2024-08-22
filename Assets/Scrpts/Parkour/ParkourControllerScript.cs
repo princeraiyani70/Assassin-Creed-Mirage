14552395a20e00a670146d4ca1587eca90540167
princeraiyani <66293545+keshavGameHead@users.noreply.github.com>
2024-08-22 08:59:35 +0530

    24

--- Assets/Scrpts/Parkour/ParkourControllerScript.cs
+++ Assets/Scrpts/Parkour/ParkourControllerScript.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class ParkourControllerScript : MonoBehaviour
 {
     public Enviromentcheaker enviromentCheaker;
+    bool playerInAction;
+    public Animator animator;
+
 
     private void Update()
     {
-        var hitData = enviromentCheaker.CheckObstacle();
-
-        if (hitData.hitFound)
+        if (Input.GetButton("Jump")  && !playerInAction)
         {
-            Debug.Log("Objcet Founded" + hitData.hitInfo.transform.name);
+            var hitData = enviromentCheaker.CheckObstacle();
+
+            if (hitData.hitFound)
+            {
+                //Debug.Log("Objcet Founded" + hitData.hitInfo.transform.name);
+                StartCoroutine(PerformParkourAction());
+            }
         }
+    }
+
+    IEnumerator PerformParkourAction()
+    {
+        playerInAction = true;
+
+        animator.CrossFade("JumpUp", 0.2f);
+        yield return null;
+
+        var animationState = animator.GetNextAnimatorStateInfo(0);
+
+        yield return new WaitForSeconds(animationState.length);
+
+        playerInAction = false;
     }
 }
