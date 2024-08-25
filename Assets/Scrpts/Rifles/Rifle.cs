1da5c83417f95e23d8adedbf54a5f0847194d216
PrinceRaiyaniGame <145953793+princeraiyani70@users.noreply.github.com>
2024-08-25 18:33:59 +0530

    65

--- Assets/Scrpts/Rifles/Rifle.cs
+++ Assets/Scrpts/Rifles/Rifle.cs
@@ -4,15 +4,111 @@ using UnityEngine;
 
 public class Rifle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Rifle Things")]
+    public Transform shootingArea;
+    public float giveDamage = 10f;
+    public float shootingRange = 100f;
+    public Animator animator;
+    public bool isMoving;
+    public PlayerScript playerScript;
+
+    [Header("Rifle Ammunition And Reloading")]
+    private int maximumAmmunition = 1;
+    public int presentAmmunition;
+    public int mag;
+    public float reloadingTime;
+    private bool setReloading;
+    public GameObject crossHair;
+
+    private void Start()
     {
-        
+        presentAmmunition = maximumAmmunition;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (animator.GetFloat("movementValue") > 0.001f)
+        {
+            isMoving = true;
+        }
+
+        else if (animator.GetFloat("movementValue") < 0.999999f)
+        {
+            isMoving = false;
+        }
+
+        if (setReloading)
+            return;
+
+        if (presentAmmunition <= 0 && mag > 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0) && isMoving==false)
+        {
+            animator.SetBool("RifleActive", true);
+            animator.SetBool("Shooting", true);
+            Shoot();
+        }
+        else if (!Input.GetMouseButtonDown(0))
+        {
+            animator.SetBool("Shooting", false);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            animator.SetBool("RifleAim", true);
+            //crossHair.SetActive(true);
+        }
+
+        else if (!Input.GetMouseButton(1))
+        {
+            animator.SetBool("RifleAim", false);
+            //crossHair.SetActive(false);
+        }
+    }
+
+    void Shoot()
+    {
+        if (mag <= 0)
+        {
+            return;
+        }
+
+        presentAmmunition--;
+
+        if (presentAmmunition == 0)
+        {
+            mag--;
+        }
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(shootingArea.position, shootingArea.forward, out hitInfo, shootingRange))
+        {
+            KnightAi knightAi = hitInfo.transform.GetComponent<KnightAi>();
+
+            if (knightAi != null)
+            {
+                knightAi.TakeDamage(giveDamage);
+            }
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        setReloading = true;
+        animator.SetFloat("movementValue", 0f);
+        playerScript.movementSpeed = 0f;
+        animator.SetBool("ReloadRifle", true);
+        //reloading Anim
+        yield return new WaitForSeconds(reloadingTime);
+        animator.SetBool("ReloadRifle", false);
+        presentAmmunition = maximumAmmunition;
+        setReloading = false;
+        animator.SetFloat("movementValue", 0f);
+        playerScript.movementSpeed = 5f;
     }
 }
